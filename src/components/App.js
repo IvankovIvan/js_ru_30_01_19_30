8b90f50885dc7d033cbe8287661375c379cafaae
Ivankov <ivan20481@gmail.com>
2017-02-12 19:37:31 +0300

    Homework

--- src/components/App.js
+++ src/components/App.js
@@ -3,30 +3,64 @@ import ArticleList from './ArticleList'
 import Chart from './Chart'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+import DayPicker, {DateUtils} from 'react-day-picker'
+import 'react-day-picker/dist/style.css'
+
 
 class App extends Component {
     state = {
         user: '',
-        selection: null
-    }
+        selection: null,
+        from: null,
+        to: null
+    };
 
     render() {
-        const {articles} = this.props
+        const { from, to } = this.state;
+        console.log(from);
+        const {articles} = this.props;
         const options = articles.map(article => ({
             label: article.title,
             value: article.id
-        }))
+        }));
         return (
             <div>
                 User: <input type="text" value={this.state.user} onChange={this.handleUserChange}/>
                 <Select options = {options} onChange={this.handleSelectChange} value={this.state.selection} multi/>
                 <ArticleList articles={articles}/>
                 <Chart articles={articles}/>
+                { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
+                { from && !to && <p>Please select the <strong>last day</strong>.</p> }
+                { from && to &&
+                <p>
+                    You chose from { from.toString() } to { to.toString() }.
+                    { ' ' }
+                    <a href="." onClick={ this.handleResetClick }>Reset</a>
+                </p>
+                }
+                <DayPicker
+                    numberOfMonths={ 1 }
+                    selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                    onDayClick={ this.handleDayClick }
+                />
             </div>
         )
     }
 
-    handleSelectChange = selection => this.setState({ selection })
+    handleDayClick = (e, day) => {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    };
+
+    handleResetClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            from: null,
+            to: null,
+        });
+    };
+
+    handleSelectChange = selection => this.setState({ selection });
 
     handleUserChange = (ev) => {
         if (ev.target.value.length < 10) {
@@ -39,6 +73,6 @@ class App extends Component {
 
 App.propTypes = {
     articles: PropTypes.array.isRequired
-}
+};
 
 export default App
