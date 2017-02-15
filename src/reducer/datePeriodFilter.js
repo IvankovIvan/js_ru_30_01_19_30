6fd4ac327d3b5d28d222daaf1b803bd19357f2c1
Roman <kaboom.ua@gmail.com>
2017-02-15 22:49:32 +0200

    Update datePeriodFilter.js

--- src/reducer/datePeriodFilter.js
+++ src/reducer/datePeriodFilter.js
@@ -1,5 +1,5 @@
 import {DATE_PERIOD_FILTER} from '../constants'
-
+//не дроби редюсеры слишком сильно: объедини селект и календарь в один
 export default (datePeriod = {from: null, to: null}, action) => {
     const {type, payload} = action;
     switch (type) {
